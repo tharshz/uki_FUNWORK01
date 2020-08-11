8287bb6c1a7c52505169bdc39fa335541d535395
Tharsha Loganathan <tharshzlog@gmail.com>
2020-08-11 12:34:56 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function () {
+    SC.stream('/tracks/805132165', function (sound) {
+        //function start for button
+        $('#start').click(function (e) {
+            e.preventDefault();
+            sound.start();
+        });
+        // stop for function
+
+        $('#stop').click(function (e) {
+            e.preventDefault();
+            sound.stop();
+        });
+
+
+    });
+
+});
