0b14270cd4433294ae7ca40555ec5d2d1342c6e7
Tharsha Loganathan <tharshzlog@gmail.com>
2020-08-11 14:14:13 +0530

    new

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ SC.initialize({
     client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 $(document).ready(function () {
-    SC.stream('/tracks/684015817', function (sound) {
+    SC.stream('/tracks/253075652', function (sound) {
         //function start for button
         $('#start').click(function (e) {
             e.preventDefault();
